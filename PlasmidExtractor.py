ffa8413f1b96017fe03ea67f6c84d1059fb05fd8
Andrew Low <andrew.low@inspection.gc.ca>
2017-10-12 16:23:02 -0400

    Command line options actually work now

--- PlasmidExtractor.py
+++ PlasmidExtractor.py
@@ -17,19 +17,20 @@ from Bio import SeqIO
 # 3) Identify contigs that are likely of plasmid origin in a draft assembly.
 
 
-def mash_fasta(assembly, plasmid_sketch, outdir='tmp/', threads=8):
+def mash_fasta(assembly, plasmid_sketch, outdir='tmp/', threads=8, logfile='logfile.log'):
     """
     :param assembly: Fasta assembly file.
     :param plasmid_sketch: .msh file created by mash sketch
     :param outdir: output directory where tsv file called mash_output containing distances for each contig in assembly
     to each plasmid in the plasmid_sketch file will be stored.
     :param threads: Number of threads to run mash on.
+    :param logfile: Log to write stdout and stderr to.
     """
     print('Mashing as initial screen.')
     # Add parallelism eventually.
     cmd = 'mash dist -d 0.1 -p {} -i {} {} > {}/mash_output'.format(str(threads), assembly, plasmid_sketch, outdir)
-    print(cmd)
-    subprocess.call(cmd, shell=True)
+    with open(logfile, 'a+') as log:
+        subprocess.call(cmd, shell=True, stdout=log, stderr=log)
 
 
 def parse_mash_output(mash_output):
@@ -58,10 +59,11 @@ def parse_mash_output(mash_output):
     return best_hits
 
 
-def make_blast_database(fasta_file):
+def make_blast_database(fasta_file, logfile='logfile.log'):
     """
     Checks that a fasta file has the files needed for it to be a blast db, and creates the db if it doesn't.
     :param fasta_file: Full path to fasta file.
+    :param logfile: Log to write stdout and stderr to.
     :return:
     """
     database_extensions = ['.nhr', '.nin', '.nsq']
@@ -71,7 +73,8 @@ def make_blast_database(fasta_file):
             db_exists = False
     if not db_exists:
         cmd = 'makeblastdb -dbtype nucl -in {}'.format(fasta_file)
-        subprocess.call(cmd, shell=True)
+        with open(logfile, 'a+') as log:
+            subprocess.call(cmd, shell=True, stdout=log, stderr=log)
 
 
 def retrieve_contig_sequence(contig_header, fasta_file):
@@ -115,13 +118,15 @@ def blast_things(best_hits, assembly):
     return plasmid_locations
 
 
-def quality_trim(forward_in, reverse_in, forward_out, reverse_out, logfile='logfile.log'):
+def quality_trim(forward_in, reverse_in, forward_out, reverse_out, logfile='logfile.log', threads=4):
     """
-    Runs bbduk to quality trim a set of reads.
+    Runs bbduk to quality trim a set of reads - also removes adapters.
     :param forward_in: Forward reads.
     :param reverse_in: Reverse reads.
     :param forward_out: Outputted forward trimmed reads.
     :param reverse_out: Outputted reverse trimmed reads.
+    :param threads: Number of threads to run bbduk with.
+    :param logfile: Log to write stdout and stderr to.
     """
     print('Quality Trimming...')
     cmd = 'which bbduk.sh'
@@ -129,13 +134,15 @@ def quality_trim(forward_in, reverse_in, forward_out, reverse_out, logfile='logf
     bbduk_dir = bbduk_dir.split('/')[:-1]
     bbduk_dir = '/'.join(bbduk_dir)
     cmd = 'bbduk.sh in1={} in2={} out1={} out2={} qtrim=w trimq=20 k=25 minlength=50 forcetrimleft=15' \
-          ' ref={}/resources/adapters.fa overwrite hdist=1 tpe tbo'.format(forward_in, reverse_in,
-                                                                           forward_out, reverse_out, bbduk_dir)
+          ' ref={}/resources/adapters.fa overwrite hdist=1 tpe tbo threads={}'.format(forward_in, reverse_in,
+                                                                                      forward_out, reverse_out,
+                                                                                      bbduk_dir, str(threads))
     with open(logfile, 'a+') as log:
         subprocess.call(cmd, shell=True, stdout=log, stderr=log)
 
 
-def get_plasmid_reads(forward_in, reverse_in, forward_out, reverse_out, plasmid_database, logfile='logfile.log'):
+def get_plasmid_reads(forward_in, reverse_in, forward_out, reverse_out, plasmid_database,
+                      logfile='logfile.log', threads=4):
     """
     Use bbduk to extract any reads that match to the plasmid database.
     :param forward_in: Forward input reads.
@@ -143,10 +150,13 @@ def get_plasmid_reads(forward_in, reverse_in, forward_out, reverse_out, plasmid_
     :param forward_out: Forward reads that match to the database.
     :param reverse_out: Reverse reads that match to the database.
     :param plasmid_database: Fasta-formatted file with sequences you want things to match to.
+    :param threads: Number of threads to run bbduk with.
+    :param logfile: Log to write stdout and stderr to.
     """
     print('Extracting plasmid reads...')
-    cmd = 'bbduk.sh in1={} in2={} outm={} outm2={} ref={} overwrite'.format(forward_in, reverse_in, forward_out,
-                                                                            reverse_out, plasmid_database)
+    cmd = 'bbduk.sh in1={} in2={} outm={} outm2={} ref={} threads={} overwrite'.format(forward_in, reverse_in,
+                                                                                       forward_out, reverse_out,
+                                                                                       plasmid_database, str(threads))
     with open(logfile, 'a+') as log:
         subprocess.call(cmd, shell=True, stdout=log, stderr=log)
 
@@ -157,6 +167,7 @@ def interleave_reads(forward_in, reverse_in, interleaved, logfile='logfile.log')
     :param forward_in: Forward input reads.
     :param reverse_in: Reverse input reads.
     :param interleaved: Interleaved read file.
+    :param logfile: Log to write stdout and stderr to.
     """
     print('Interleaving!')
     cmd = 'reformat.sh in1={} in2={} out={} overwrite'.format(forward_in, reverse_in, interleaved)
@@ -164,7 +175,8 @@ def interleave_reads(forward_in, reverse_in, interleaved, logfile='logfile.log')
         subprocess.call(cmd, shell=True, stdout=log, stderr=log)
 
 
-def generate_consensus(reads, reference_fasta, output_fasta, logfile='logfile.log', cleanup=True, output_base='out'):
+def generate_consensus(reads, reference_fasta, output_fasta, logfile='logfile.log',
+                       cleanup=True, output_base='out', threads=4):
     """
     Generates a consensus fasta give a set of interleaved reads and a reference sequence.
     :param reads: Interleaved reads.
@@ -173,13 +185,15 @@ def generate_consensus(reads, reference_fasta, output_fasta, logfile='logfile.lo
     :param logfile: Log to write stdout and stderr to.
     :param cleanup: If true, deletes intermediate files.
     :param output_base: Base name for output files.
+    :param threads: Number of threads to run analysis with.
     """
     with open(logfile, 'a+') as log:
         # Step 1: Index fasta file
         cmd = 'samtools faidx {}'.format(reference_fasta)
         subprocess.call(cmd, shell=True, stderr=log, stdout=log)
         # Step 2: Run bbmap to generate sam/bamfile.
-        cmd = 'bbmap.sh ref={} in={} out={} nodisk overwrite'.format(reference_fasta, reads, output_base + '.bam')
+        cmd = 'bbmap.sh ref={} in={} out={} nodisk overwrite threads={}'.format(reference_fasta, reads,
+                                                                                output_base + '.bam', str(threads))
         subprocess.call(cmd, shell=True, stderr=log, stdout=log)
         # Step 3: Sort the bam file.
         cmd = 'samtools sort {}.bam -o {}_sorted.bam'.format(output_base, output_base)
@@ -203,7 +217,33 @@ def generate_consensus(reads, reference_fasta, output_fasta, logfile='logfile.lo
             os.remove(item)
 
 
-def kmerize_reads(read_file, output_file, kmer_size=31, logfile='logfile.log'):
+def remove_plasmid_from_reads(plasmid_sequences, forward_in, reverse_in, forward_out, reverse_out,
+                              tmpfasta, logfile='logfile.log', threads=4):
+    """
+    Removes reads that have kmers matching to plasmid sequence.
+    :param plasmid_sequences: List of plasmid fasta files. Will be concatenated into 1 fasta.
+    :param forward_in: Forward input reads.
+    :param reverse_in: Reverse input reads.
+    :param forward_out: Forward output reads.
+    :param reverse_out: Reverse output reads.
+    :param tmpfasta: Where your temporary concatenated fasta will be stored.
+    :param logfile: Log to write stdout and stderr to.
+    :param threads: Number of threads to run analysis with.
+    :return:
+    """
+    print('Generating plasmid-less reads...')
+    with open(tmpfasta, 'w') as outfile:
+        for sequence in plasmid_sequences:
+            with open(sequence) as infile:
+                outfile.write(infile.read())
+    cmd = 'bbduk.sh ref={} in={} in2={} out={} out2={} threads={} overwrite'.format(tmpfasta, forward_in, reverse_in,
+                                                                                    forward_out, reverse_out,
+                                                                                    str(threads))
+    with open(logfile, 'a+') as log:
+        subprocess.call(cmd, shell=True, stdout=log, stderr=log)
+
+
+def kmerize_reads(read_file, output_file, kmer_size=31, logfile='logfile.log', threads=4):
     """
     Generates kmers using jellyfish on a read file.
     :param read_file: Read file. Can be single-end or interleaved.
@@ -212,55 +252,10 @@ def kmerize_reads(read_file, output_file, kmer_size=31, logfile='logfile.log'):
     :param logfile: Logfile to send stdout and stderr to.
     """
     with open(logfile, 'a+') as log:
-        cmd = 'jellyfish count -m {} -t 12 -s 100M --bf-size 100M -C {}'.format(str(kmer_size), read_file)
+        cmd = 'jellyfish count -m {} -t {} -s 100M --bf-size 100M -C {}'.format(str(kmer_size), str(threads), read_file)
         subprocess.call(cmd, shell=True, stdout=log, stderr=log)
         cmd = 'jellyfish dump -c mer_counts.jf > ' + output_file
         subprocess.call(cmd, shell=True, stdout=log, stderr=log)
-
-
-def only_assembly(assembly, output_base):
-    tmpdir = output_base + 'tmp/'
-    if not os.path.isdir(output_base):
-        os.makedirs(output_base)
-    if not os.path.isdir(tmpdir):
-        os.makedirs(tmpdir)
-    print('Analyzing draft assembly.')
-    # Step 1: Mash each contig against the plasmid database sketch to see what's most closely related and screen out
-    # things we don't want to blast against.
-    mash_fasta(assembly, 'reduced_sketch.msh', outdir=tmpdir)
-    hits = parse_mash_output(tmpdir + '/mash_output')
-    # Step 2: Once MASH has done a preliminary screen, get to BLASTing to more precisely locate plasmid sequences.
-    locations = blast_things(hits, assembly)
-    # Step 3: Create a nice report that tells you everything you need to know.
-    f = open(output_base + '/plasmid_locations.csv', 'w')
-    f.write('Contig,Plasmid,Start,End\n')
-    for contig in hits:
-        for location in locations[contig]:
-            outstr = '{},{},{},{}\n'.format(contig, hits[contig], str(location[0]), str(location[1]))
-            f.write(outstr)
-    f.close()
-
-
-def only_reads(reads, output_base):
-    tmpdir = output_base + 'tmp/'
-    if not os.path.isdir(output_base):
-        os.makedirs(output_base)
-    print('Analyzing raw reads!')
-    # Step 1: Quality trim reads.
-    quality_trim(reads[0], reads[1], tmpdir + 'trimmed_R1.fastq.gz', tmpdir + 'trimmed_R2.fastq.gz')
-    # Step 1.5: Bait out reads that match to plasmid stuff.
-    get_plasmid_reads(tmpdir + 'trimmed_R1.fastq.gz', tmpdir + 'trimmed_R2.fastq.gz',
-                      tmpdir + 'plasmid_R1.fastq.gz', tmpdir + 'plasmid_R2.fastq.gz', 'nucleotideseq.fa')
-    interleave_reads(tmpdir + 'plasmid_R1.fastq.gz', tmpdir + 'plasmid_R2.fastq.gz', tmpdir + 'interleaved.fastq')
-    # Step 2: MASH reads against plasmid sketch to figure out which plasmids we should be reference mapping.
-    # mash_reads('interleaved.fastq.gz', 'plasmid_sketch.msh')
-    kmerize_reads(tmpdir + 'interleaved.fastq', tmpdir + 'reads.tab')
-    plasmids = kmer_overlap.find_plasmids(tmpdir + 'reads.tab')
-    # plasmids = parse_read_mash_output('tmp/mash_output')
-    # Step 3: For each very likely plasmid, do reference mapping and generate a consensus sequence.
-    for plasmid in plasmids:
-        generate_consensus(tmpdir + 'interleaved.fastq', plasmid.replace('kmerized_plasmids', 'reduced_db'),
-                           output_base + '/' + os.path.split(plasmid)[-1] + '_consensus.fasta')
 
 
 class PlasmidExtractor(object):
@@ -284,13 +279,16 @@ class PlasmidExtractor(object):
         elif self.assembly == 'NA' and self.forward_reads != 'NA':
             self.only_reads()
         elif self.assembly != 'NA' and self.forward_reads == 'NA':
-            only_assembly(self.assembly, self.output_base)
+            self.only_assembly()
         else:
             print('Input is both raw reads and assembly. I have not fully figured out what it is that I want to do with'
                   ' this yet.')
 
-        if not self.keep_tmpfiles:
-            shutil.rmtree(self.output_base + 'tmp')
+        try:
+            if not self.keep_tmpfiles:
+                shutil.rmtree(self.output_base + 'tmp')
+        except FileNotFoundError:
+            pass
 
     def only_reads(self):
         tmpdir = self.output_base + 'tmp/'
@@ -299,25 +297,34 @@ class PlasmidExtractor(object):
         print('Analyzing raw reads!')
         # Step 1: Quality trim reads.
         quality_trim(self.forward_reads, self.reverse_reads, tmpdir + 'trimmed_R1.fastq.gz',
-                     tmpdir + 'trimmed_R2.fastq.gz', logfile=self.logfile)
+                     tmpdir + 'trimmed_R2.fastq.gz', logfile=self.logfile, threads=self.threads)
         # Step 1.5: Bait out reads that match to plasmid stuff.
         get_plasmid_reads(tmpdir + 'trimmed_R1.fastq.gz', tmpdir + 'trimmed_R2.fastq.gz',
                           tmpdir + 'plasmid_R1.fastq.gz', tmpdir + 'plasmid_R2.fastq.gz', 'nucleotideseq.fa',
-                          logfile=self.logfile)
+                          logfile=self.logfile, threads=self.threads)
         interleave_reads(tmpdir + 'plasmid_R1.fastq.gz', tmpdir + 'plasmid_R2.fastq.gz', tmpdir + 'interleaved.fastq',
                          logfile=self.logfile)
         # Step 2: MASH reads against plasmid sketch to figure out which plasmids we should be reference mapping.
         # mash_reads('interleaved.fastq.gz', 'plasmid_sketch.msh')
-        kmerize_reads(tmpdir + 'interleaved.fastq', tmpdir + 'reads.tab', logfile=self.logfile)
+        kmerize_reads(tmpdir + 'interleaved.fastq', tmpdir + 'reads.tab', logfile=self.logfile, threads=self.threads)
         print('Searching for plasmids in raw reads...')
         plasmids = kmer_overlap.find_plasmids(tmpdir + 'reads.tab', threads=self.threads)
         # plasmids = parse_read_mash_output('tmp/mash_output')
         # Step 3: For each very likely plasmid, do reference mapping and generate a consensus sequence.
+        finished_plasmids = list()
+        print('Found {} potential plasmids!'.format(len(plasmids)))
         for plasmid in plasmids:
             print('Generating consensus for {}-like sequence...'.format(plasmid))
             generate_consensus(tmpdir + 'interleaved.fastq', plasmid.replace('kmerized_plasmids', 'reduced_db'),
                                self.output_base + '/' + os.path.split(plasmid)[-1] + '_consensus.fasta',
-                               logfile=self.logfile)
+                               logfile=self.logfile, threads=self.threads)
+            finished_plasmids.append(self.output_base + '/' + os.path.split(plasmid)[-1] + '_consensus.fasta')
+        # Step 4: Generate raw reads that don't have plasmid reads (ideally).
+        remove_plasmid_from_reads(finished_plasmids, self.forward_reads, self.reverse_reads,
+                                  self.output_base + '/' + self.forward_reads.replace('.fastq', '_noplasmid.fastq'),
+                                  self.output_base + '/' + self.reverse_reads.replace('.fastq', '_noplasmid.fastq'),
+                                  tmpdir + 'concatenated_plasmid.fasta', threads=self.threads)
+        print('Plasmid extraction complete!')
 
     def only_assembly(self):
         tmpdir = self.output_base + 'tmp/'
@@ -340,6 +347,7 @@ class PlasmidExtractor(object):
                 outstr = '{},{},{},{}\n'.format(contig, hits[contig], str(location[0]), str(location[1]))
                 f.write(outstr)
         f.close()
+        print('Plasmid locating complete!')
 
 
 if __name__ == '__main__':
@@ -358,15 +366,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     extractor = PlasmidExtractor(args)
     extractor.main()
-    """
-    if args.assembly == 'NA' and args.reads == 'NA':
-        print('No assembly files or raw reads specified. Exiting...')
-        sys.exit()
-    elif args.assembly == 'NA' and args.reads != 'NA':
-        only_reads(args.reads, args.output_dir)
-    elif args.assembly != 'NA' and args.reads == 'NA':
-        only_assembly(args.assembly, args.output_dir)
-    else:
-        print('Input is both raw reads and assembly. I have not fully figured out what it is that I want to do with '
-              'this yet.')
-    """
+
